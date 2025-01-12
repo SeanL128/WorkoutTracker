7df920582f10927f2f7f80732d6b312dc35d9222
SeanL128 <seanlindsay2008@gmail.com>
2025-01-12 16:20:13 -0500

    Initial Commit

---
+++ WorkoutTrackerTests/WorkoutTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerTests.swift
+//  WorkoutTrackerTests
+//
+//  Created by Sean Lindsay on 1/12/25.
+//
+
+import Testing
+@testable import WorkoutTracker
+
+struct WorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
